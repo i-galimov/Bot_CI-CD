5c1d0ad804b61c726d99fdc122b3eb26177e7f85
Ilshat <ilshat999.galimov@ya.ru>
2023-10-15 16:21:50 +0300

    delete c in exxxcuse

--- start_pod.sh
+++ start_pod.sh
@@ -6,7 +6,7 @@ kubectl expose deployment resume-bot --type=NodePort --port=8080
 kubectl create deployment translate-bot --image phella/translate_bot
 kubectl expose deployment translate-bot --type=NodePort --port=8081
 
-kubectl create deployment exxxcusebot --image phella/exxxcusebot
-kubectl expose deployment exxxcusebot --type=NodePort --port=8082
+kubectl create deployment exxxusebot --image phella/exxxusebot
+kubectl expose deployment exxxusebot --type=NodePort --port=8082
 
 kubectl get pod
--- stop_pod.sh
+++ stop_pod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kubectl delete services translate-bot resume-bot
-kubectl delete deployment translate-bot resume-bot
+kubectl delete services translate-bot resume-bot exxxusebot
+kubectl delete deployment translate-bot resume-bot exxxusebot
 minikube stop
